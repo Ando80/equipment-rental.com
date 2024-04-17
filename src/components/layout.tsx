2f2ccf68529eb1eaa4b3e4aa14c3d14ebba0f9cd
ando <ando@exponent.team>
2024-04-17 17:38:59 +0300

    authentication

---
+++ src/components/layout.tsx
@@ -0,0 +1,7 @@
+// defini les bases de layouts
+
+import { twx } from "@/lib/twx";
+
+export const Layout = twx.div((props) => [
+  `max-w-5xl w-full flex gap-4 mx-auto px-4`,
+]);
---
+++ src/features/layout/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { LoggedInButton } from "../auth/LoggedInButton";
+import { Layout } from "@/components/layout";
+import { ModeToggle } from "../theme/ModeToggle";
+
+export const Header = async () => {
+  return (
+    <header className="w-full border-b border-border py-1">
+      <Layout className="flex items-center gap-4">
+        <div className="flex-1">
+          <Image src="/icon.png" width={50} height={50} alt="icon" />
+        </div>
+        <div className="flex items-center gap-2">
+          <ModeToggle />
+          <LoggedInButton />
+        </div>
+      </Layout>
+    </header>
+  );
+};
